51fd9141bcea93c20c196caf3ab10b10917cac41
sumandinda007 <sumandinda123@gmail.com>
2017-11-01 00:25:29 +0530

    First commit From Ubuntu

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://foodoku.in/mobileapp/api",
+	'DialogDefaultTitle' : "Foodoku",
+	'pushNotificationSenderid' : "973044842925",
+	'facebookAppId' : "851850738291440",
+	'APIHasKey' : "88ee6952dab7af0363d233510ce07b7fd00e04769bf4f123cadc5234df1d7922"
+};
